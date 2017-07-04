148d60f8e7805c95b1959a7a4b6f6fdab5db7109
Nik Nyby <nnyby@columbia.edu>
2017-07-04 17:45:30 -0400

    exercise four

---
+++ four.ml
@@ -0,0 +1,16 @@
+open Core.Std
+
+let reverse s =
+  let len = String.length s in
+  String.init len ~f:(fun i -> s.[len - 1 - i])
+
+let is_palindrome s = reverse s = s
+
+let () =
+  for i = 900 to 999 do
+    for j = 900 to 999 do
+      if is_palindrome (string_of_int (i * j))
+      then printf "%d * %d = %d" i j (i * j)
+      else printf ":PPPPpppp"
+    done
+  done
