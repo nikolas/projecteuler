2f441d718e8b528b5fbbee71583cb2ab370430a7
Nik Nyby <nikolas@gnu.org>
2018-01-27 19:43:35 -0500

    add java dir

---
+++ java/One.java
@@ -0,0 +1,9 @@
+/**
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+
+public class One {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
