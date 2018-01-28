4a68bfba58e70714942756b5a8dc2801c7b70670
Nik Nyby <nikolas@gnu.org>
2018-01-27 19:49:19 -0500

    add One.java

--- java/One.java
+++ java/One.java
@@ -3,7 +3,24 @@
  */
 
 public class One {
+    /*
+     * Returns true if n is multiple of 3 or 5.
+     */
+    static boolean isMultiple(int n) {
+        return n % 3 == 0 || n % 5 == 0;
+    }
+
+    static int sumMultiplesTo(int n) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (isMultiple(i)) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        System.out.println(sumMultiplesTo(1000));
     }
 }
