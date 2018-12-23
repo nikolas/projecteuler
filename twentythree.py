15d63832ddfc57aac39939bb0a2c05a32b3edc63
Nikolas Nyby <nikolas@gnu.org>
2018-12-23 00:04:06 -0500

    okay

--- twentythree.py
+++ twentythree.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import pickle
+from os.path import exists
+
 def is_divisor(x, y):
     if y == 0:
         return False
@@ -25,34 +28,45 @@ def find_abundants_to(x):
     return nums
 
 def limit_abundants(x, abundants):
-    cutoff = len(abundants)
-    for i in abundants:
-        if i > x:
-            cutoff = i
-            break
+    for idx, i in enumerate(abundants):
+        if i >= x:
+            return idx
 
-    return abundants[:cutoff]
-
+    return len(abundants)
 
 def is_abundant_sum(x, abundants):
-    abundants = limit_abundants(x, abundants)
-    for i in abundants:
-        for j in range(len(abundants)):
-            el = abundants[-j]
-            if i != el and (i + el) == x:
+    if x > 100 and x % 2 == 0:
+        return True
+
+    abundants = abundants[:limit_abundants(x, abundants)]
+    for idx, i in enumerate(abundants):
+        for j in abundants[idx:]:
+            if (i + j) == x and i != j:
                 return True
 
     return False
 
 def find_non_abundant_nums_to(x):
     nums = []
-    abundants = find_abundants_to(x)
+    abundants = []
+
+    fname = 'abundants.txt'
+    if True or not exists(fname):
+        print('Finding abundants...')
+        abundants = find_abundants_to(x)
+        with open(fname, 'wb') as f:
+            pickle.dump(abundants, f)
+    else:
+        print('Loading abundants from {}'.format(fname))
+        with open(fname, 'rb') as f:
+            abundants = pickle.load(f)
+
     for i in range(x):
         if not is_abundant_sum(i, abundants):
-            print('appending %d' % i)
+            print('{} is non-abundant.'.format(i))
             nums.append(i)
 
     return nums
 
 if __name__ == '__main__':
-    print(sum(find_non_abundant_nums_to(28123)))
+    print(sum(find_non_abundant_nums_to(16000)))
